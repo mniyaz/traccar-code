ec7a8237f14e8688bcfc79289506aca4ddf0740e
mohemmedniyaz <mohemmedniyaz@greenorange.in>
2019-03-17 20:42:44 +0530

    Code

---
+++ src/DeviceList.js
@@ -0,0 +1,61 @@
+import React, { Component, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { updateDevices } from './actions';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import Avatar from '@material-ui/core/Avatar';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Divider from '@material-ui/core/Divider';
+
+const mapStateToProps = state => ({
+  devices: state.devices
+});
+
+class DeviceList extends Component {
+  componentDidMount() {
+    
+  }
+
+  render() {
+	  fetch('/api/devices').then(response => {
+      if (response.ok) {
+        console.log('response', response)
+		  
+        response.json().then(devices => {
+          this.props.dispatch(updateDevices(devices));
+		  
+        });
+      }
+    });
+    const devices = this.props.devices.map(device =>
+      <Fragment key={device.id.toString()}>
+        <ListItem button>
+          <Avatar>
+            <LocationOnIcon />
+          </Avatar>
+          <ListItemText primary={device.name} secondary={device.uniqueId} />
+          <ListItemSecondaryAction>
+            <IconButton>
+              <MoreVertIcon />
+            </IconButton>
+          </ListItemSecondaryAction>
+        </ListItem>
+        <li>
+          <Divider variant="inset" />
+        </li>a
+      </Fragment>
+    );
+
+    return (
+      <List>
+        {devices}
+      </List>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(DeviceList);
---
+++ src/Drivers.js
@@ -0,0 +1,122 @@
+import React, { Component } from 'react';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import ContainerDimensions from 'react-container-dimensions';
+import MainToobar from './MainToolbar';
+import MainMap from './MainMap';
+import Drawer from '@material-ui/core/Drawer';
+import withStyles from '@material-ui/core/styles/withStyles';
+import SocketController from './SocketController';
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
+import DeviceList from './DeviceList';
+
+
+            
+
+const styles = theme => ({
+  root: {
+   flex: 1
+  },
+  content: {
+    flexGrow: 1,
+    overflow: "hidden",
+    display: "flex",
+    flexDirection: "row",
+    [theme.breakpoints.down('xs')]: {
+      flexDirection: "column-reverse"
+    }
+  },
+  drawerPaper: {
+    position: 'relative',
+    
+      width: 350,
+    
+    
+      height: 250
+    
+  },
+  marginLeft: {
+marginLeft: 10
+  },
+  margin: {
+margin: 20
+  },
+});
+
+
+class MainPage extends Component {
+  constructor(props) {
+const data  = {"id":12,"name":"Kevin","uniqueId":12345,"atributes":""};
+    super(props);
+    this.state = {
+loading: true,
+    driverJson: {"id":12,"name":"Kevin","uniqueId":12345,"atributes":""},
+        };
+  }
+
+  componentDidMount() {
+    fetch('/api/session').then(response => {
+      if (response.ok) {
+        this.setState({
+          loading: false
+        });
+      } else {
+        this.props.history.push('/login');
+      }
+    });
+fetch('/api/drivers', {
+method: 'POST',
+headers: {
+'Content-Type': 'application/json',
+},
+body: JSON.stringify(this.state.driverJson)
+})
+    .then(function(response) {
+        //return response.json()
+      }).then(function(body) {
+        console.log("response body::"+body);
+      });
+
+  }
+
+  render() {
+    const { classes } = this.props;
+    const { loading } = this.state;
+    if (loading) {
+      return (
+        <div>Loading...</div>
+      );
+    } else {
+      return (
+<div className={classes.root}>
+<SocketController />
+<MainToobar history={this.props.history} />
+<div className={classes.margin}>
+<div className={classes.marginLeft}>
+<label>
+Name:
+<input type="text" value={this.state.value} />
+</label>
+</div>
+<div class="form-group">
+                <label >Name:</label>
+                   <select class="form-control">
+                            <option>Any</option>
+                            <option>On Route</option>
+                            <option>Not on Route</option>
+                          </select>
+                        </div>
+                        <div class="form-group">
+                          <label   >Employee ID:</label>
+                          <select class="form-control">
+                            <option>Any</option>
+                          </select>
+                        </div>
+</div>
+</div>
+        
+      );
+    }
+  }
+}
+
+export default withWidth()(withStyles(styles)(MainPage));
